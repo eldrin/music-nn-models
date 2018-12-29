734c91939df52392c9019bd93a229e0b12362970
Jaehun Kim <j.h.kim@tudelft.nl>
2018-12-29 16:23:22 +0100

    added SegmentationNet (UNet-Style)

--- musicnn/models/__init__.py
+++ musicnn/models/__init__.py
@@ -1,4 +1,7 @@
 from .autotagging import VGGlike2DAutoTagger
 from .autoencoder import VGGlike2DAutoEncoder
+from .segmentation import VGGlike2DSkipSegmentator
 
-__all__ = ['VGGlike2DAutoTagger', 'VGGLike2DAutoEncoder']
+__all__ = ['VGGlike2DAutoTagger',
+           'VGGlike2DAutoEncoder',
+           'VGGlike2DSkipSegmentator']
---
+++ musicnn/models/segmentation.py
@@ -0,0 +1,69 @@
+from functools import partial
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+from .layers import VGGlike2DEncoder, VGGlike2DDecoder, Identity
+from .architecture import STFTInputNetwork
+
+
+class VGGlike2DUNetSegmentator(STFTInputNetwork):
+    """VGG-like 2D segmentation (learning masking)
+    """
+    def __init__(self, sig_len=44100, n_hidden=256, n_fft=1024, hop_sz=256,
+                 layer1_channels=8, kernel_size=3, n_convs=1,
+                 pooling=nn.MaxPool2d, pool_size=2, non_linearity=nn.ReLU,
+                 global_average_pooling=True, batch_norm=True, dropout=0.5,
+                 normalization='standard'):
+        """"""
+        super().__init__(sig_len, n_hidden, batch_norm, dropout, n_fft, hop_sz,
+                         magnitude=True, log=True, normalization=normalization)
+
+        # initialize the encoder
+        self.E = VGGlike2DEncoder(
+            self.input_shape, n_hidden, n_convs, layer1_channels,
+            kernel_size, pooling, pool_size, non_linearity,
+            global_average_pooling, batch_norm
+        )
+
+        # put on some decision (prediction) layers on top of it
+        self.P = nn.Sequential(
+            nn.Linear(n_hidden, n_hidden),
+            self.hid_bn(), non_linearity(), self._dropout(),
+        )
+
+        # put decoder on top of it
+        self.iP = nn.Sequential(
+            nn.Linear(n_hidden, n_hidden),
+            self.hid_bn(), non_linearity(), self._dropout()
+        )
+
+        # put decoder for convolution encoders
+        self.D = VGGlike2DDecoder(self.E)
+
+    def get_hidden_state(self, x, layer=10):
+        return self.E.get_hidden_state(self._preproc(x), layer)
+
+    def get_bottleneck(self, x):
+        return self.P(self.E(self._preproc(x)))
+
+    def forward(self, x):
+        Z = []  # for skip-connection
+
+        # Encoding
+        z = X = self._preproc(x)
+        for layer in self.E.encoder:
+            z = layer(z)
+            Z.append(z)
+        
+        # bottleneck
+        z = self.iP(self.P(z))
+
+        # Decoding
+        for iz, layer in zip(Z[::-1], self.D.decoder):
+            z = layer(z + iz)
+        Xhat = z
+
+        return X, Xhat
