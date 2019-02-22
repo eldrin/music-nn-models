4815119425d023200ec83651f333a1643180b2c5
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2019-02-22 14:34:01 +0100

    updated MFCC Vocal separator

--- musicnn/models/__init__.py
+++ musicnn/models/__init__.py
@@ -1,10 +1,11 @@
 from .autotagging import VGGlike2DAutoTagger, ShallowAutoTagger, MFCCAutoTagger
 from .autoencoder import VGGlike2DAutoEncoder, MFCCAutoEncoder
-from .sourceseparation import VGGlike2DUNet
+from .sourceseparation import VGGlike2DUNet, MFCCAESourceSeparator
 
 __all__ = ['VGGlike2DAutoTagger',
            'VGGlike2DAutoEncoder',
            'VGGlike2DUNet',
            'ShallowAutoTagger',
            'MFCCAutoTagger',
-           'MFCCAutoEncoder']
+           'MFCCAutoEncoder',
+           'MFCCSourceSeparator']
--- musicnn/models/sourceseparation.py
+++ musicnn/models/sourceseparation.py
@@ -6,7 +6,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as f
 
-from .layers import VGGlike2DEncoder, VGGlike2DDecoder, Identity, STFT, SameConv2D
+from .layers import (MFCCEncoder,
+                     VGGlike2DEncoder,
+                     VGGlike2DDecoder,
+                     Identity,
+                     STFT,
+                     SameConv2D)
 from .architecture import STFTInputNetwork
 from ..config import Config as cfg
 
@@ -92,6 +97,114 @@ class VGGlike2DUNet(STFTInputNetwork):
             else:
                 zv = layer_v(zv, idx)
 
+        return torch.sigmoid(zv)
+
+    def forward(self, x):
+        # preprocessing
+        X, z = self._preproc(x)  # scaled / not scaled
+
+        # get mask
+        M = self.get_mask(z)
+
+        return M * X
+
+    def _post_process(self, Xm, Xp=None):
+        """Inverse magnitude to signal
+
+        NOTE: currently only support where original phase is provided
+
+        Args:
+            Xm (tensor): db-scale magnitude (output of forward of this model)
+            Xp (numpy.ndarray): original phase
+
+        Returns:
+            x (numpy.ndarray): time-domain signal
+        """
+        assert Xp is not None
+
+        # to array
+        if Xm.is_cuda:
+            Xm = Xm.data.cpu().numpy()  # also remove channel dim
+        else:
+            Xm = Xm.data.numpy()
+
+        # to signal (n_batch, sig_len)
+        x = np.array([librosa.istft(X) for X in Xm[0] * np.exp(1j * Xp)])
+
+        return x
+
+    
+class MFCCAESourceSeparator(STFTInputNetwork):
+    """2D auto-tagger with fully on-line MFCC encoder
+    """
+    def __init__(self, sig_len=44100, n_mfccs=40, sr=22050,
+                 n_fft=1024, hop_sz=256, non_linearity=nn.ReLU,
+                 batch_norm=True, dropout=0.5):
+        """"""
+        self.n_hidden = n_mfccs * 6  # with stats and deltas
+
+        super().__init__(sig_len, self.n_hidden, batch_norm, dropout,
+                         n_fft, hop_sz, magnitude=True, log=False,
+                         normalization=False)
+
+        # initialize the encoder
+        self.E = MFCCEncoder(n_mfccs, n_fft, sr)
+
+
+        # initialize the encoder
+        _E = VGGlike2DEncoder(
+            self.input_shape, self.n_hidden,
+            n_convs=1, layer1_channels=16, kernel_size=3,
+            pooling=nn.AvgPool2d, pool_size=2,
+            non_linearity=non_linearity,
+            global_average_pooling=True,
+            batch_norm=True, rtn_pool_idx=False
+        )
+
+        # put on some decision (prediction) layers on top of it
+        self.P = nn.Sequential(
+            self.hid_bn(), non_linearity(), self._dropout(),
+        )
+
+        # put decoder on top of it
+        self.iPv = nn.Sequential(
+            nn.Linear(self.n_hidden, self.n_hidden),
+            self.hid_bn(), non_linearity(), self._dropout()
+        )
+
+        # put decoder for convolution encoders
+        self.Dv = VGGlike2DDecoder(_E)
+
+    def get_hidden_state(self, x, layer=10):
+        return self.E.get_hidden_state(self._preproc(x), layer)
+
+    def get_bottleneck(self, x):
+        # preprocessing
+        _, z = self._preproc(x)  # scaled / not scaled
+        z = self.E(z)
+        
+        # bottleneck
+        z = self.P(z)
+        return z
+
+    def _preproc(self, x):
+        X = torch.stft(x, self.stft.n_fft, self.stft.hop_sz,
+                       window=self.stft.window)
+        X = self.stft._magnitude(X)
+
+        Z = self.sclr(self.stft._log(X))[:, None]
+        return X[:, None], Z
+
+    def get_mask(self, z):
+        Z = []  # for skip-connection
+        z = self.E(z)
+
+        # bottleneck
+        z = self.P(z)
+        zv = self.iPv(z)
+
+        # decoding
+        zv = self.Dv(zv, [None] * len(self.Dv.decoder)) 
         return torch.sigmoid(zv)
 
     def forward(self, x):
